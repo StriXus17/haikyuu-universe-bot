c1b4f061ee82b5ac5ca759227499c467fbc31a77
StriXus17 <56758874+StriXus17@users.noreply.github.com>
2021-08-18 19:32:57 +0100

    Update app.py

--- haikyuu-middle/app.py
+++ haikyuu-middle/app.py
@@ -36,6 +36,6 @@ print(response_dialogflow)
 # @app.route("/routeName")
 # def functionName():
 #    return render_template("fileName.html")
-
+port = int(os.environ.get("PORT", 5000))
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=port)
