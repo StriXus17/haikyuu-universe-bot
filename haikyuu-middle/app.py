69586b340b670a1642925f728fff862c8cf15d2a
StriXus17 <56758874+StriXus17@users.noreply.github.com>
2021-08-19 09:45:32 +0100

    Update app.py

--- haikyuu-middle/app.py
+++ haikyuu-middle/app.py
@@ -34,6 +34,7 @@ def result():
     text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
     query_input = dialogflow.types.QueryInput(text=text_input)
     response_dialogflow = session_client.detect_intent(session=session, query_input=query_input)
+    return json.dump(response_dialogflow)
 
     # Here's how you create a route
     # @app.route("/routeName")
