11e6b2d79c78300e6e38e5ad098df122e96c193b
StriXus17 <56758874+StriXus17@users.noreply.github.com>
2021-08-21 08:56:37 +0100

    Update app.py

--- haikyuu-middle/app.py
+++ haikyuu-middle/app.py
@@ -17,32 +17,35 @@ def contact():
     return render_template("contact.html")
 @app.route('/', methods=['POST'])
 def result():
-    global fyodor
-    global dazai
-    print(request.json['query_input']['text']['text'])
-    fyodor = request.json['query_input']['text']['text']
-    dazai = request.json['query_input']['text']['id']
-    print("I'm here")
-    with open('private_key.json', "w") as myfile:
-        myfile.write(request.json['query_input']['text']['private-key'])
-        myfile.close()
-    f = open("private_key.json", "r")
-    print(f.read())
-    import dialogflow
-    import random
-    import string
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "private_key.json"
-    project_id = dazai
-    session_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
-    language_code = "en"
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-    text = fyodor
-    text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.types.QueryInput(text=text_input)
-    response_dialogflow = session_client.detect_intent(session=session, query_input=query_input)
-    json_response = MessageToJson(response_dialogflow)
-    return json_response
+    if 'id' in ['query_input']['text'].keys():
+        global fyodor
+        global dazai
+        print(request.json['query_input']['text']['text'])
+        fyodor = request.json['query_input']['text']['text']
+        dazai = request.json['query_input']['text']['id']
+        print("I'm here")
+        f = open("private_key.json", "r")
+        print(f.read())
+        import dialogflow
+        import random
+        import string
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "private_key.json"
+        project_id = dazai
+        session_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
+        language_code = "en"
+        session_client = dialogflow.SessionsClient()
+        session = session_client.session_path(project_id, session_id)
+        text = fyodor
+        text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        response_dialogflow = session_client.detect_intent(session=session, query_input=query_input)
+        json_response = MessageToJson(response_dialogflow)
+        return json_response
+    else:
+        with open('private_key.json', "w") as myfile:
+            myfile.write(json.dumps(request.json))
+            myfile.close()
+        
 
     # Here's how you create a route
     # @app.route("/routeName")
