959d73758f370e6538b2c3a4c905e5211680e8cb
StriXus17 <56758874+StriXus17@users.noreply.github.com>
2021-08-21 13:19:50 +0100

    Update app.py

--- haikyuu-middle/app.py
+++ haikyuu-middle/app.py
@@ -17,7 +17,7 @@ def contact():
     return render_template("contact.html")
 @app.route('/', methods=['POST'])
 def result():
-    if 'id' in ['query_input']['text'].keys():
+    if 'id' in request.json['query_input']['text'].keys():
         global fyodor
         global dazai
         print(request.json['query_input']['text']['text'])
